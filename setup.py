66bbfa475af8ad0c40bbe4dffbacd42a26df4777
Daniel Graña <dangra@gmail.com>
2015-08-13 23:16:14 -0300

    modernize setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='loginform',
-      version='1.0',  # also update loginform.__version__
-      license='BSD',
-      description='Fill HTML login forms automatically',
-      author='Scrapinghub',
-      author_email='info@scrapinghub.com',
-      url='http://github.com/scrapy/loginform',
-      platforms=['Any'],
-      py_modules=['loginform'],
-      install_requires=['lxml'],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          ],
-      )
+
+setup(
+    name='loginform',
+    version='1.0',  # also update loginform.__version__
+    license='BSD',
+    description='Fill HTML login forms automatically',
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    url='http://github.com/scrapy/loginform',
+    platforms=['Any'],
+    py_modules=['loginform'],
+    install_requires=['lxml'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
